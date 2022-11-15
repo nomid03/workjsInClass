d0614a15fcb53914a9dce3cd0eca67ff7005b63e
Nematullah Omid <98613991+nomid03@users.noreply.github.com>
2022-11-15 10:12:33 +0100

    I work with list thnigs and adding three of them

---
+++ index.js
@@ -0,0 +1,11 @@
+let thN ='computer';
+let thN1 = 'muse';
+let thN2 = 'bag';
+let thP = 100;
+let thp1 = 100;
+let thP2 =100;
+let totalp = thP+thp1+thP2;
+let di =(totalp)+(6/100);
+alert(thN+thP+'-'+thN1+thp1+'-'+thN2+thP2+'-'+'sumtotal'+totalp )
+alert(di);
+
